b7356318aae382510818ef9f07fd2452d7926ca8
Moslem Khanbeiki <s.khanbeiki@gmail.com>
2025-07-01 14:19:38 +0330

    add library

--- wearclothingstore/src/main/java/ir/khanbeiki/wearclothingstore/presentation/MainActivity.kt
+++ wearclothingstore/src/main/java/ir/khanbeiki/wearclothingstore/presentation/MainActivity.kt
@@ -8,22 +8,19 @@ package ir.khanbeiki.wearclothingstore.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
-import ir.khanbeiki.wearclothingstore.R
 import ir.khanbeiki.wearclothingstore.presentation.theme.KmpClothingStoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -57,12 +54,19 @@ fun WearApp(greetingName: String) {
 
 @Composable
 fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
+    Button(onClick = {
+
+    }) {
+        Text(
+            text = "reza"
+        )
+    }
+//    Text(
+//        modifier = Modifier.fillMaxWidth(),
+//        textAlign = TextAlign.Center,
+//        color = MaterialTheme.colors.primary,
+//        text = stringResource(R.string.hello_world, greetingName)
+//    )
 }
 
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
